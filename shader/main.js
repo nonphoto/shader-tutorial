248399820090bec4d2101e0899b97715da90b007
Jonas Luebbers <jonaskluebbers@gmail.com>
2017-11-12 14:38:44 -0500

    Move utility functions to separate file

--- shader/main.js
+++ shader/main.js
@@ -1,61 +1,4 @@
-function createContext(canvas) {
-  const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
-  if (gl) {
-    return gl
-  }
-  else {
-    throw 'WebGLNotSupported'
-  }
-}
-
-function createProgram(gl, vertexShader, fragmentShader) {
-  const program = gl.createProgram()
-  gl.attachShader(program, vertexShader)
-  gl.attachShader(program, fragmentShader)
-  gl.linkProgram(program)
-
-  if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    return program
-  }
-  else {
-    console.error("Unable to initialize the shader program: " + gl.getProgramInfoLog(program))
-    throw 'ProgramInitializationException'
-  }
-}
-
-function createShader(gl, source, type) {
-  const shader = gl.createShader(type)
-  gl.shaderSource(shader, source)
-  gl.compileShader(shader)
-
-  if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    return shader
-  }
-  else {
-    gl.deleteShader(shader)
-    throw new Error('Shader compilation failed: ' + gl.getShaderInfoLog(shader))
-  }
-}
-
-function request(path) {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest()
-    request.open('GET', path)
-    request.responseType = 'text'
-
-    request.onload = () => {
-      if (request.status === 200) {
-        resolve(request.response)
-      }
-      else {
-        reject(Error('Unable to load path'))
-      }
-    }
-
-    request.send()
-  })
-}
-
+// Vertex coordinates corresponding to the edges of the canvas in shader-space.
 const vertexPositions = [
 	-1, -1, 0,
 	-1, 1, 0,
@@ -68,8 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	canvas.width = 800
 	canvas.height = 600
 
-  const vertexSource = request('vertex.glsl')
-  const fragmentSource = request('fragment.glsl')
+	const vertexSource = request('vertex.glsl')
+	const fragmentSource = request('fragment.glsl')
 
 	Promise.all([vertexSource, fragmentSource]).then(([vertexSource, fragmentSource]) => {
 		const gl = createContext(canvas)
@@ -87,6 +30,10 @@ document.addEventListener('DOMContentLoaded', () => {
 		const vertexPositionAttribute = gl.getAttribLocation(program, "vertexPosition")
 		const resolutionUniform = gl.getUniformLocation(program, "resolution")
 
+		gl.uniform2f(resolutionUniform, canvas.width, canvas.height)
+
+		gl.enableVertexAttribArray(vertexPositionAttribute)
+
 		const vertexBuffer = gl.createBuffer()
 		gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPositions), gl.STATIC_DRAW)
@@ -94,9 +41,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		function draw() {
 			gl.clear(gl.COLOR_BUFFER_BIT)
 
-			gl.uniform2f(resolutionUniform, canvas.width, canvas.height)
-
-			gl.enableVertexAttribArray(vertexPositionAttribute)
 			gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0)
 
 			gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
---
+++ shader/util.js
@@ -0,0 +1,70 @@
+/**
+* Create a new WebGL context. Throws an error if WebGL is not supported.
+*/
+function createContext(canvas) {
+    const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl")
+    if (gl) {
+        return gl
+    }
+    else {
+        throw 'WebGLNotSupported'
+    }
+}
+
+/**
+* Create a new WebGL program from a vertex and fragment shader pair.
+*/
+function createProgram(gl, vertexShader, fragmentShader) {
+    const program = gl.createProgram()
+    gl.attachShader(program, vertexShader)
+    gl.attachShader(program, fragmentShader)
+    gl.linkProgram(program)
+
+    if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        return program
+    }
+    else {
+        console.error("Unable to initialize the shader program: " + gl.getProgramInfoLog(program))
+        throw 'ProgramInitializationException'
+    }
+}
+
+/**
+* Create a new WebGL shader object.
+*/
+function createShader(gl, source, type) {
+    const shader = gl.createShader(type)
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+
+    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        return shader
+    }
+    else {
+        gl.deleteShader(shader)
+        throw new Error('Shader compilation failed: ' + gl.getShaderInfoLog(shader))
+    }
+}
+
+/**
+* Request the file at the given path. Returns a promise that resolves to
+* a string of the file's contents.
+*/
+function request(path) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
+        request.open('GET', path)
+        request.responseType = 'text'
+
+        request.onload = () => {
+            if (request.status === 200) {
+                resolve(request.response)
+            }
+            else {
+                reject(Error('Unable to load path'))
+            }
+        }
+
+        request.send()
+    })
+}
