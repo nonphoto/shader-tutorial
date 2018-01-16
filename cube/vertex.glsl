c26f5edc2f89104970f0f764a288a3161d68cb33
Jonas Luebbers <jonaskluebbers@gmail.com>
2018-01-15 21:52:31 -0500

    Clean up cube vertex shader

--- cube/vertex.glsl
+++ cube/vertex.glsl
@@ -1,5 +1,4 @@
 attribute vec3 a_vertexPosition;
-attribute vec4 a_vertexColor;
 
 uniform mat4 u_model;
 uniform mat4 u_view;
@@ -7,10 +6,7 @@ uniform mat4 u_projection;
 
 varying vec4 v_color;
 
-vec3 color;
-
 void main() {
-	color = (a_vertexPosition * 0.5) + 0.5;
-	v_color = vec4(color, 1.0);
+	v_color = vec4((a_vertexPosition * 0.5) + 0.5, 1.0);
 	gl_Position = u_projection * u_view * u_model * vec4(a_vertexPosition, 1.0);
 }
