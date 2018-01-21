37b2633ed3298579c6bb55dfebfb0c87f9b8dad5
Jonas Luebbers <jonaskluebbers@gmail.com>
2018-01-21 15:49:58 -0500

    Clean up HTML

--- cube/main.js
+++ cube/main.js
@@ -19,55 +19,53 @@ const elements = [].concat(
 	[0, 4, 5], [0, 1, 5]
 )
 
-document.addEventListener('DOMContentLoaded', () => {
-	const canvas = document.getElementById('canvas')
-	canvas.width = 1024
-	canvas.height = 1024
+const canvas = document.getElementById('canvas')
+canvas.width = 1024
+canvas.height = 1024
 
-	const vertexSource = request('vertex.glsl')
-	const fragmentSource = request('fragment.glsl')
+const vertexSource = request('vertex.glsl')
+const fragmentSource = request('fragment.glsl')
 
-	Promise.all([vertexSource, fragmentSource]).then(([vertexSource, fragmentSource]) => {
-		const gl = createContext(canvas)
+Promise.all([vertexSource, fragmentSource]).then(([vertexSource, fragmentSource]) => {
+	const gl = createContext(canvas)
 
-		gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
+	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
 
-		const vertexShader = createShader(gl, vertexSource, gl.VERTEX_SHADER)
-		const fragmentShader = createShader(gl, fragmentSource, gl.FRAGMENT_SHADER)
+	const vertexShader = createShader(gl, vertexSource, gl.VERTEX_SHADER)
+	const fragmentShader = createShader(gl, fragmentSource, gl.FRAGMENT_SHADER)
 
-		const program = createProgram(gl, vertexShader, fragmentShader)
-		gl.useProgram(program)
+	const program = createProgram(gl, vertexShader, fragmentShader)
+	gl.useProgram(program)
 
-		gl.clearColor(1.0, 0.0, 1.0, 1.0)
-		gl.clear(gl.COLOR_BUFFER_BIT)
+	gl.clearColor(1.0, 0.0, 1.0, 1.0)
+	gl.clear(gl.COLOR_BUFFER_BIT)
 
-		gl.enable(gl.DEPTH_TEST)
+	gl.enable(gl.DEPTH_TEST)
 
-		const vertexPositionAttribute = gl.getAttribLocation(program, 'a_vertexPosition')
-		const timeUniform = gl.getUniformLocation(program, 'u_time')
+	const vertexPositionAttribute = gl.getAttribLocation(program, 'a_vertexPosition')
+	const timeUniform = gl.getUniformLocation(program, 'u_time')
 
-		const vertexBuffer = gl.createBuffer()
+	const vertexBuffer = gl.createBuffer()
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+
+	const elementBuffer = gl.createBuffer()
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementBuffer)
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(elements), gl.STATIC_DRAW)
+
+	function draw(t) {
+		gl.uniform1f(timeUniform, t)
+
+		gl.enableVertexAttribArray(vertexPositionAttribute)
 		gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+		gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0)
 
-		const elementBuffer = gl.createBuffer()
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementBuffer)
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(elements), gl.STATIC_DRAW)
 
-		function draw(t) {
-			gl.uniform1f(timeUniform, t)
+		gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0)
+		requestAnimationFrame(draw)
+	}
 
-			gl.enableVertexAttribArray(vertexPositionAttribute)
-			gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-			gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0)
-
-			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, elementBuffer)
-
-			gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0)
-			requestAnimationFrame(draw)
-		}
-
-		draw(performance.now())
-	})
+	draw(performance.now())
 })
 
