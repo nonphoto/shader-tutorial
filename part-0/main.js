c3dfdf082d3bfdf7ada36d332f8ec4de88dcd980
Jonas Luebbers <jonaskluebbers@gmail.com>
2018-01-21 21:23:52 -0500

    Add part 1

--- part-0/main.js
+++ part-0/main.js
@@ -1,26 +1,18 @@
-// Get the canvas element.
+// Get the canvas element and set the resolution.
 const canvas = document.getElementById('canvas')
 canvas.width = 1024
 canvas.height = 1024
 
-// Request shader source files.
+// Request the shdaer source files, they get returned as strings.
 const vertexSource = request('vertex.glsl')
 const fragmentSource = request('fragment.glsl')
 
-// Wait for shader files to load.
+// Wait for the source files to load.
 Promise
 .all([vertexSource, fragmentSource])
 .then(([vertexSource, fragmentSource]) => {
 
-	// Initialize some things here.
+	// Draw something in here eventually.
 
-	function draw(t) {
-
-		// Draw some things in here.
-
-		window.requestAnimationFrame(draw)
-	}
-
-	draw(performance.now())
 })
 
---
+++ part-1/main.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById('canvas')
+canvas.width = 1024
+canvas.height = 1024
+
+const vertexSource = request('vertex.glsl')
+const fragmentSource = request('fragment.glsl')
+
+Promise
+.all([vertexSource, fragmentSource])
+.then(([vertexSource, fragmentSource]) => {
+
+	// Create a new WebGL context for the canvas element.
+	const gl = createContext(canvas)
+
+	// Set the dimensions of the viewport equal to the resolution. For example pixels
+	// will be numbered horizontally from 0 to 800 instead of -1 to 1.
+	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
+
+	// Create new shader objects from the vertex and fragment shader sources.
+	const vertexShader = createShader(gl, vertexSource, gl.VERTEX_SHADER)
+	const fragmentShader = createShader(gl, fragmentSource, gl.FRAGMENT_SHADER)
+
+	// Create a new program with the two shaders.
+	const program = createProgram(gl, vertexShader, fragmentShader)
+
+	// Tell the GPU to use the program we just made.
+	gl.useProgram(program)
+
+	// Set the clear color to a fetching pink for debugging purposes.
+	gl.clearColor(1.0, 0.0, 1.0, 1.0)
+
+	// Clear the canvas. This sets every pixel to the clear color.
+	gl.clear(gl.COLOR_BUFFER_BIT)
+})
+
